38038c4007f8d69505c2c3f4fcc4604fb89e6b14
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-18 13:50:57 +0300

    new config

--- config/project.py
+++ config/project.py
@@ -1,4 +1,1 @@
-project_name = "demos-md"
-project_long_description = "Demos for the Markdown language"
-project_year_started = 2020
-project_description = """Demos for the Markdown language"""
+description_short = "Demos for the Markdown language"
